ad9d364ed1d8a73cd066f731d1b9b2a97e50c763
Ray Di Ciaccio <ray@appcues.com>
2016-05-12 20:13:36 -0400

    Implement create functions.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-  export function createElement() {}
-  export function createElementNS() {}
-  export function createTextNode() {}
-  export function appendChild() {}
-  export function removeChild() {}
-  export function insertBefore() {}
-  export function parentNode() {}
-  export function nextSibling() {}
-  export function tagName() {}
-  export function setTextContent() {}
+export function createElement(tagName) {
+    return document.createElement(tagName);
+}
+
+export function createElementNS(namespaceURI, tagName) {
+    return document.createElementNS(namespaceURI, tagName);
+}
+
+export function createTextNode(text) {
+    return document.createTextNode(text);
+}
+
+export function appendChild() {}
+export function removeChild() {}
+export function insertBefore() {}
+export function parentNode() {}
+export function nextSibling() {}
+export function tagName() {}
+export function setTextContent() {}
--- test/tests/index_test.js
+++ test/tests/index_test.js
@@ -13,4 +13,32 @@ describe("snabbdom-iframe-domapi", () => {
         expect(api.tagName).to.be.a('function');
         expect(api.setTextContent).to.be.a('function');
     });
+
+    describe("createElement", () => {
+        it("should create a DOM Element", () => {
+            const el = api.createElement('iframe');
+            expect(el).to.have.property('nodeName', 'IFRAME');
+            expect(el).to.be.an.instanceOf(Element);
+        });
+    });
+
+    describe("createElementNS", () => {
+        it("should create a DOM Element with the specified namespace", () => {
+            const el = api.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            expect(el).to.have.property('nodeName', 'svg');
+            expect(el).to.have.property('namespaceURI', 'http://www.w3.org/2000/svg');
+            expect(el).to.be.an.instanceOf(Element);
+        });
+    });
+
+    describe("createTextNode", () => {
+        it("should create a DOM Text node", () => {
+            const node = api.createTextNode('snabbdom!');
+            expect(node).to.have.property('textContent', 'snabbdom!');
+            expect(node).to.have.property('nodeType', Node.TEXT_NODE);
+            expect(node).to.be.an.instanceOf(Node);
+        });
+    });
+
+
 });
